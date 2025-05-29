7f6f7c90d2829df39d05d81802e15398fc359379
unknown <macaulim.official@gmail.com>
2025-05-29 09:24:44 -0300

    modified

--- dev/app.py
+++ dev/app.py
@@ -1,27 +1,24 @@
 import requests
-from bs4 import BeautifulSoup 
-def fetch_page():
-    url = 'https://produto.mercadolivre.com.br/MLB-3925753517-console-sony-playstation-5-slim-digital-branco-1tb-jogos-ratchet-and-clank-e-returnal-_JM#polycard_client=recommendations_vpp-pdp-v2p-pom&reco_backend=ranker_retrieval_system_vpp_v2p&reco_model=ranker_entity_v2_retrieval_system_vpp_v2p&reco_client=vpp-pdp-v2p-pom&reco_item_pos=0&reco_backend_type=low_level&reco_id=3e834eea-ba24-4619-92f2-0b4e48069f93'
-    response = requests.get(url)
-    return response.text
 
-def parse_page(html):
-    soup = BeautifulSoup(html, 'html.parser')
-    product_name = soup.find('h1', class_='ui-pdp-title').get_text(strip=True)
-    prices = soup.find_all('span', class_='andes-money-amount__fraction')
-    old_price = int(prices[0].get_text(strip=True).replace('.', ''))
-    new_price = int(prices[1].get_text(strip=True).replace('.', ''))
-    installment_price = int(prices[2].get_text(strip=True).replace('.', ''))
-
-    return {
-        'product_name': product_name,
-        'old_price': old_price,
-        'new_price': new_price,
-        'installment_price': installment_price
-    }
-
+def get_dollar_quote():
+    url = 'https://economia.awesomeapi.com.br/json/last/USD-BRL'
+    try:
+        response = requests.get(url)
+        response.raise_for_status()
+        data = response.json()
+        dolar_info = data['USDBRL']
+        result = {
+            'moeda': 'Dólar',
+            'codigo': dolar_info['code'],
+            'cotacao': float(dolar_info['bid']),
+            'alta': float(dolar_info['high']),
+            'baixa': float(dolar_info['low']),
+            'data': dolar_info['create_date']
+        }
+        return result
+    except Exception as e:
+        return {'erro': 'Não foi possível obter a cotação.', 'detalhes': str(e)}
 
 if __name__ == '__main__':
-    page_content = fetch_page()
-    product_info = parse_page(page_content)
-    print(product_info)
+    cotacao = get_dollar_quote()
+    print(cotacao)
